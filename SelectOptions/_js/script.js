e5a52bfa6628e09c38459caa7faef0e8469ecd76
MIgHenSen <128144438+MIgHenSen@users.noreply.github.com>
2023-08-09 08:50:10 -0300

    Add files via upload

---
+++ SelectOptions/_js/script.js
@@ -0,0 +1,5 @@
+function numeroMes(){
+    let meses = document.getElementById("mesesDoAno");
+    let i = meses.selectedIndex;
+    let mesEscolhido = meses.options[i].value;
+    document.getElementById("t1").innerHTML = "Mês Selecionado: " + mesEscolhido; }
