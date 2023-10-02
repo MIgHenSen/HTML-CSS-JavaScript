df367cec87caf55585661f09fb7c7563b3a1f77e
MIgHenSen <128144438+MIgHenSen@users.noreply.github.com>
2023-10-02 10:39:18 -0300

    Delete SelectOptions directory

--- SelectOptions/_js/script.js
+++
@@ -1,5 +0,0 @@
-function numeroMes(){
-    let meses = document.getElementById("mesesDoAno");
-    let i = meses.selectedIndex;
-    let mesEscolhido = meses.options[i].value;
-    document.getElementById("t1").innerHTML = "Mês Selecionado: " + mesEscolhido; }
