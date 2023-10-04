c8f6d922f7152be2c732dc8259eb5ba2e212f625
MIgHenSen <128144438+MIgHenSen@users.noreply.github.com>
2023-10-04 10:17:30 -0300

    Delete SelectOptions directory

--- SelectOptions/_js/script.js
+++
@@ -1,5 +0,0 @@
-function numeroMes(){ // Função atribuida ao botão html
-    let meses = document.getElementById("mesesDoAno"); // variavel local, que recebe valores do select com id mesesDoAno
-    let i = meses.selectedIndex; // Variaveel local, que recebe opção do select escolhida pelo usuário
-    let mesEscolhido = meses.options[i].value; //Variavel local, que recebe valor da opção escolhida pelo usuário
-    document.getElementById("t1").innerHTML = "Mês Selecionado: " + mesEscolhido; } // Exibe numero do mes ao usuário
