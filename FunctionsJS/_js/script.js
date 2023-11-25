a42a83ea4e6d5b04169cd22337529d266fb7b1bf
MIgHenSen <128144438+MIgHenSen@users.noreply.github.com>
2023-11-24 22:38:59 -0300

    Update script.js

--- FunctionsJS/_js/script.js
+++ FunctionsJS/_js/script.js
@@ -1,6 +1,4 @@
 function somar(){
-    /* 'parseFloat' converte os valores recebidos em números decimais */
-     /* 'getElementById' acessa os atributos de um elemento com um id */
     let n1 = parseFloat(document.getElementById("n1").value);
     let n2 = parseFloat(document.getElementById("n2").value);
     let resultado = n1+n2;
@@ -26,8 +24,6 @@ function dividir(){
 }
 
 function multiplicar(){
-    /* 'parseFloat' converte os valores recebidos em números decimais */
-    /* 'getElementById' acessa os atributos de um elemento com um id */
     let n1 = parseFloat(document.getElementById("n1").value);
     let n2 = parseFloat(document.getElementById("n2").value);
     let resultado = n1*n2;
